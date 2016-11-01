be36dfc33dd3bd845bb05a7f905348609b99b89a
Jithin Chand Vadlamudi <jithinchandbtech@gmail.com>
2016-11-01 14:01:26 +0100

    Added rest call to get persons list from swapi

--- js/app.js
+++ js/app.js
@@ -15,14 +15,36 @@ var searchApp = angular.module('StarWarsSearchApp', ['ngRoute'])
                 redirectTo: '/'
             });
     })
-    .factory('personsListFactory', function ($http) {
+    .factory('personsListFactory', function ($http, $q) {
         return {
             getPersonsList: function() {
-                return $http.get('http://swapi.co/api/people/').then(function(response) {
-                    return response.data;
+                return $http.get('http://swapi.co/api/people').then(function(response) {
+                    var count = response.data.count;
+                    var lastPage = 1;
+                    if (count % 10 == 0)
+                        lastPage = 0;
+                    var totPages = count/10 + lastPage;
+                    var promiseResults = [];
+                    var results = [];
+                    for (var i = 1; i <= totPages; i++) {
+                        promiseResults.push(getResource('http://swapi.co/api/people/?page=' + i));
+                    }
+
+                    return $q.all(promiseResults).then(function (response) {
+                        for (var res in response) {
+                            results = results.concat(response[parseInt(res)]);
+                        }
+                        return results;
+                    });
                 });
             }
         };
+
+        function getResource(url) {
+            return $http.get(url).then(function(response) {
+                return response.data.results;
+            });
+        }
     })
     .controller('SearchCtrl', function ($scope, personsList) {
         $scope.personsData = personsList;
